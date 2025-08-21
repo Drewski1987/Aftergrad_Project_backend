b1e13e624ecf71f1148829b618ea8e920a58861c
Maurice Drew <drewm1987@gmail.com>
2025-08-20 20:43:12 -0500

    Need to debug user_id constraint violation

--- api/pets.js
+++ api/pets.js
@@ -4,7 +4,27 @@ import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken";
 import { getPets } from "#db/queries/pets";
 
-router.get("/pets", async (req, res)=>{
-    const pets = await getPets();
-    res.send(pets);
+
+
+router.post("/pets", async (req, res, next)=>{
+    const {name, note, imageUrl, user_id} = req.body
+
+    if (!name || !note || !imageUrl || !user_id) {
+        return res.status(400).send("Missing required body")
+    }
+    const newPet = await createPet ({
+        name: name,
+        note: note,
+        imageUrl: imageUrl,
+        user_id: req.user.id
+    })
+    res.status(200).send(newPet);
 });
+
+router.get("/pets", async (req, res, next)=>{
+    const pets = await getPets({user_id: req.user.id});
+    res.status(200).send(pets);
+});
+
+router.post("/:id")
+
--- db/queries/pets.js
+++ db/queries/pets.js
@@ -7,14 +7,34 @@ export async function createPets({name, note, imageUrl, user_id}) {
     VALUES ($1, $2, $3, $4)
     RETURNING *;
     `;
-    const {rows:pet} = await db.query(sql, [name, note, imageUrl, user_id]);
-    return pet[0]
+    const {rows:pets} = await db.query(sql, [name, note, imageUrl, user_id]);
+    return pets[0]
 }
 
-export async function getPets(){
+export async function getPets({user_id}){
     const sql = `
-    SELECT * FROM pets;
+    SELECT * FROM pets WHERE user_id = $1;
     `;
-    const {rows:pets} = await db.query(sql);
-    return pets;
+    const {rows:pets} = await db.query(sql, [user_id]);
+    return pets[0];
+}
+
+// export async function getPet(id){
+//     const sql = `
+//     SELECT * FROM pets WHERE id = $1
+//     `;
+//     const {rows: pet} = await db.query(sql, [id])
+//     return pet[0];
+// }
+
+export async function updatePet ({name, note, imageUrl, user_id}){
+    const sql = `
+    UPDATE pets
+    SET name = $1, note = $2, imageUrl = $3, 
+    WHERE user_id = $4
+    RETURNING *; 
+    `;
+    const {rows: pet} = await db.query(sql, [name, note, imageUrl, user_id])
+    return pet;
+    
 }
